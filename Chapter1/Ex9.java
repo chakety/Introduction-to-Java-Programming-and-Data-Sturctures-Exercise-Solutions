64aee93841a40c972c68e34b3167609538e54fbd
Wenyi (Chakety) <64045577+chakety@users.noreply.github.com>
2022-10-14 16:41:19 -0400

    Create Ex9.java

---
+++ Chapter1/Ex9.java
@@ -0,0 +1,16 @@
+/*
+Write a program that displays the area and perimeter of a rectangle with a width of 4.5 and a height of 7.9 using the following formula:
+          area = width * height
+*/
+
+package practice;
+
+public class Ex1_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(2*(4.5+7.9));
+		System.out.println(4.5*7.9);
+	}
+
+}
