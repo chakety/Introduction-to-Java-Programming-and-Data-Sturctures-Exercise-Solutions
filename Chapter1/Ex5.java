0dfac04dccc1149fbd62454cf762f0e548a75971
Wenyi (Chakety) <64045577+chakety@users.noreply.github.com>
2022-10-14 16:35:45 -0400

    Create Ex5.java

---
+++ Chapter1/Ex5.java
@@ -0,0 +1,20 @@
+/*
+ Write a program that displays the result of
+        9.5 * 4.5 - 2.5 * 3
+       ---------------------
+            45.5 - 3.5 
+*/
+
+
+
+package practice;
+
+public class Ex1_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double result = ((9.5*4.5-2.5*3) / (45.5-3.5));
+		System.out.printf("%.2f%n",result);
+	}
+
+}
