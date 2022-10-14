c7094cb856f35869178fef7943095837a9dbba8d
Wenyi (Chakety) <64045577+chakety@users.noreply.github.com>
2022-10-14 16:40:08 -0400

    Create Ex8.java

---
+++ Chapter1/Ex8.java
@@ -0,0 +1,20 @@
+/*
+  Write a program that displays the area and perimeter of a circle that has a radius of 5.5 using the following formulas:
+    perimeter = 2 * radius * p
+    area = radius * radius * p
+*/
+
+package practice;
+
+public class Ex1_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double perimeter = 2 * 5.5 * 3.145;
+		double area = 5.5 * 5.5 *3.145;
+		 System.out.println(perimeter);
+		 System.out.println(area);
+	}
+
+}
+
