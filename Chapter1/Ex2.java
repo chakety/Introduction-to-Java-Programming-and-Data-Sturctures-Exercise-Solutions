cba87ef9c397de16d32c27f71f427deddbb3b6f0
Wenyi (Chakety) <64045577+chakety@users.noreply.github.com>
2022-10-14 16:28:26 -0400

    Create Ex2.java

---
+++ Chapter1/Ex2.java
@@ -0,0 +1,16 @@
+/*
+Write a program that displays "Welcome to Java"  five times.
+*/
+
+package practice;
+
+public class Ex1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i < 5; i ++) {
+			System.out.println("Welcome to Java");
+		}
+	}
+
+}
