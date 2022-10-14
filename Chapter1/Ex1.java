c66a1a327a3e0fd1662fbf328660ccf7cc260deb
Wenyi (Chakety) <64045577+chakety@users.noreply.github.com>
2022-10-14 16:21:29 -0400

    Update Ex1.java

--- Chapter1/Ex1.java
+++ Chapter1/Ex1.java
@@ -1,1 +1,18 @@
+/*
+Write a program that displays:
+  "Welcome to Java"
+  "Welcome to Computer Science"
+  "Programming is fun"
+*/
+package practice;
 
+public class Ex1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+	}
+
+}
